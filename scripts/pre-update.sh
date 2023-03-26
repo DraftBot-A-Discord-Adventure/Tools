fa45b75364ffd26c3750f1dd3c3f890fb9471d27
Feiryn <98115666+feiryn@users.noreply.github.com>
2023-03-26 18:34:13 +0200

    Call maintenance in pre update script

--- scripts/pre-update.sh
+++ scripts/pre-update.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+serverIP=http://127.0.0.1:8080
+
 # Try every 10s 60 times, so 10 min
 count=0
 while [[ $count < 60 ]]
 do
     # Curl the number of blocked player in the bot
-    if [ "$(curl -s http://127.0.0.1:8080/blocked_players)" == "0" ]
+    if [ "$(curl -s $serverIP/blocked_players)" == "0" ]
     then
         break
+    fi
+    if [ $count == 0 ]
+    then
+        curl -s -X POST -H "Content-Type: text/plain" $serverIP/maintenance?enable=1
     fi
     count=$((count+1))
     sleep 10
