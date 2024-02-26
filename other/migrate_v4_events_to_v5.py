27cf7b47b76038ba38775e9d6e683ac04cc558c5
Feiryn <98115666+feiryn@users.noreply.github.com>
2024-02-26 01:29:29 +0100

    Update events migration tool

--- other/migrate_v4_events_to_v5.py
+++ other/migrate_v4_events_to_v5.py
@@ -38,9 +38,12 @@ for filename in os.listdir(dirPath):
                 else:
                     newName = "end"
                 newEventJson["possibilities"][newName] = {}
+                newEventJson["possibilities"][newName]["outcomes"] = {}
+                if "condition" in jsonObj["possibilities"][possibilityKey]:
+                    newEventJson["possibilities"][newName]["condition"] = jsonObj["possibilities"][possibilityKey]["condition"]
                 for i in range(0, len(jsonObj["possibilities"][possibilityKey]["outcomes"])):
-                    newEventJson["possibilities"][newName][str(i)] = jsonObj["possibilities"][possibilityKey]["outcomes"][i]
-                    del newEventJson["possibilities"][newName][str(i)]["translations"]
+                    newEventJson["possibilities"][newName]["outcomes"][str(i)] = jsonObj["possibilities"][possibilityKey]["outcomes"][i]
+                    del newEventJson["possibilities"][newName]["outcomes"][str(i)]["translations"]
 
             with open(os.path.join(outputDir, filename), "w") as outputFh:
                 outputFh.write(json.dumps(newEventJson, ensure_ascii=False, indent=4, sort_keys=True))
